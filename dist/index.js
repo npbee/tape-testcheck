3531fb2ffe160367d3d125fda96f27be61a259fe
Nick Ball <nicholaspball@gmail.com>
2015-10-16 11:07:21 -0700

    build dist

--- dist/index.js
+++ dist/index.js
@@ -18,25 +18,38 @@ var _testcheck = require('testcheck');
 
 var _testcheck2 = _interopRequireDefault(_testcheck);
 
-_tapeLibTest2['default'].prototype.check = function (gens, prop, opts, msg) {
+/**
+ * Run a testcheck check function on the given propertyFn, generators, and
+ * options.
+ *
+ * @param {Array} generators An array of testcheck generators
+ * @param {Function} properyFn The property function to test.
+ * @param {Object} options An option object for testcheck options.
+ * @param {String} msg The message to pass to the Tape methods on pass/fail.
+ *
+ */
+_tapeLibTest2['default'].prototype.check = function (generators, propertyFn, options, msg) {
     if (this._skip) {
         return this.end();
     }
 
-    if (typeof opts === 'string') {
-        msg = opts;
-        opts = {};
+    if (typeof options === 'string') {
+        msg = options;
+        options = {};
     }
 
     this.emit('prerun');
 
     try {
-        var result = _testcheck2['default'].check(_testcheck2['default'].property(gens, prop), opts);
+        var result = _testcheck2['default'].check(_testcheck2['default'].property(generators, propertyFn), options);
 
         if (result.result) {
             this.pass(msg);
         } else {
-            this.fail(JSON.stringify(result, null, 4));
+            this.fail(msg, {
+                expected: { result: true },
+                actual: result
+            });
         }
     } catch (err) {
         this.error(err);
