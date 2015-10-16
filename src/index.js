ad4f4fd9c5a2fdca524c0a931dc49fd6bc13b932
Nick Ball <nicholaspball@gmail.com>
2015-10-16 11:01:41 -0700

    add documentation

--- src/index.js
+++ src/index.js
@@ -2,18 +2,28 @@ import Test from 'tape/lib/test';
 import test from 'tape';
 import testcheck from 'testcheck';
 
-Test.prototype.check = function (gens, prop, opts, msg) {
+/**
+ * Run a testcheck check function on the given propertyFn, generators, and
+ * options.
+ *
+ * @param {Array} generators An array of testcheck generators
+ * @param {Function} properyFn The property function to test.
+ * @param {Object} options An option object for testcheck options.
+ * @param {String} msg The message to pass to the Tape methods on pass/fail.
+ *
+ */
+Test.prototype.check = function (generators, propertyFn, options, msg) {
     if (this._skip) { return this.end(); }
 
-    if (typeof opts === 'string') {
-        msg = opts;
-        opts = {};
+    if (typeof options === 'string') {
+        msg = options;
+        options = {};
     }
 
     this.emit('prerun');
 
     try {
-        let result = testcheck.check(testcheck.property(gens, prop), opts);
+        let result = testcheck.check(testcheck.property(generators, propertyFn), options);
 
         if (result.result) {
             this.pass(msg);
